2ba8629a22a1ba06a0977b4d2085878211d5ca39
DaniKard <kajothada@gmail.com>
2020-12-10 15:21:01 +0200

    add animal class it's method and interface

---
+++ src/main/java/Animal.java
@@ -0,0 +1,53 @@
+//public class Animal {
+//}
+import java.util.List;
+import org.sql2o.*;
+
+
+public class Animal implements AnimalInt {
+    private String name;
+    private int id;
+
+    public Animal(String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object otherAnimal){
+        if (!(otherAnimal instanceof Animal)) {
+            return false;
+        } else {
+            Animal newAnimal = (Animal) otherAnimal;
+            return this.getName().equals(newAnimal.getName());
+
+        }
+    }
+    @Override
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name) VALUES (:name)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static List<Animal> all() {
+        String sql = "SELECT * FROM animals";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Animal.class);
+        }
+    }
+
+}
+
---
+++ src/main/java/AnimalInt.java
@@ -0,0 +1,4 @@
+public interface AnimalInt {
+    void save();
+}
+
