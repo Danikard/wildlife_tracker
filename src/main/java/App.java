3884cb05ae9cc30772b2ad91fe7cb62171413486
DaniKard <kajothada@gmail.com>
2020-12-13 12:59:42 +0200

    insert dependency

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+//public class App {
+//}
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
